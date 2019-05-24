a34c25afc1d37c59fef697d639930ce4fbcbe3ce
tjurrw <clrnr88@gmail.com>
2019-05-24 14:36:32 +0900

    14th commit

--- src/main/java/selenium_test.java
+++ src/main/java/selenium_test.java
@@ -1,23 +1,25 @@
+import java.io.File;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class selenium_test {
     public static void seleniumTest(){
         try {
-            System.setProperty("webdriver.chrome.driver", "/Users/parkchikook/IdeaProjects/exe/chromedriver");
-            WebDriver driver = new ChromeDriver();
+            System.setProperty("webdriver.gecko.driver", "/Users/parkchikook/IdeaProjects/selenium_test/geckodriver");
+            WebDriver driver = new FirefoxDriver();
 
-            driver.get("www.google.com");
-            Thread.sleep(5000);  // Let the user actually see something!
+            driver.get("http://www.google.com");
 
-            WebElement searchBox = driver.findElement(By.name("q"));
-            searchBox.sendKeys("ChromeDriver");
-            searchBox.submit();
-            Thread.sleep(5000);  // Let the user actually see something!
-            driver.quit();
-        } catch (InterruptedException ex){
+            Thread.sleep(5000);
+
+            driver.close();
+
+        } catch(InterruptedException e){
 
         }
     }
